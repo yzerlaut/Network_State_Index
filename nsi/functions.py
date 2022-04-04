a185cad5c71a1d8b0019388f3c2ed67efc79e3c4
Yann Zerlaut <yann.zerlaut@gmail.com>
2022-04-04 12:52:30 +0200

    adding synthetic example plot

--- nsi/functions.py
+++ nsi/functions.py
@@ -1,12 +1,6 @@
-import sys, pathlib
-sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 import numpy as np
 from scipy import signal
 from scipy.ndimage.filters import gaussian_filter1d
-
-Blue, Orange, Green, Red, Purple, Brown, Pink, Grey,\
-    Kaki, Cyan = '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',\
-    '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'
 
 ##############################################
 ########### Wavelet Transform ################
@@ -57,13 +51,22 @@ def norm_constant_th(freq, dt, w0=6.):
     n = (w0/2./np.sqrt(2.*np.pi)/freq)*(1.+np.exp(-w0**2/2))
     return n/dt
 
+
+def compute_freq_envelope(signal, sampling_freq, freqs):
+    """
+    compute the frequency power using wavelet transform
+
+    1. performs wavelet transform of the signal
+    2. transform to envelope only (absolute value)
+    3. take the maximum over the considered band
+    """
+    return np.max(np.abs(my_cwt(signal,
+                                freqs,
+                                1./sampling_freq)), axis=0) # max of envelope of wavelet freq.
+
 ##################################################
 ########### Processing of the LFP ################
 ##################################################
-
-def gaussian_smoothing(Signal, idt_sbsmpl=10.):
-    """Gaussian smoothing of the data"""
-    return gaussian_filter1d(Signal, idt_sbsmpl)
 
 def heaviside(x):
     """ heaviside (step) function """
@@ -78,109 +81,111 @@ def compute_pLFP(LFP, sampling_freq,
     performs continuous wavelet transform and smooth the time-varying high-gamma freq power
     """
 
-    if len(LFP.shape)>1:
-        # we compute the pLFP on each channel and sum
-        W = np.zeros(LFP.shape[1])
-        for i in range(LFP.shape[0]):
-            W += np.abs(my_cwt(LFP[i,:], freqs, 1./sampling_freq)).mean(axis=0)
-        W /= LFP.shape[0]
-    else:
-        # performing wavelet transform and taking the mean power over the frequency content considered
-        W = np.abs(my_cwt(LFP, freqs, 1./sampling_freq)).mean(axis=0)
-
+    W = compute_freq_envelope(LFP, sampling_freq, freqs)
+    
     isubsmpl = int(new_dt*sampling_freq)
     
     # then smoothing and subsampling
-    pLFP = gaussian_smoothing(np.reshape(W[:int(len(W)/isubsmpl)*isubsmpl],
+    pLFP = gaussian_filter1d(np.reshape(W[:int(len(W)/isubsmpl)*isubsmpl],
                                          (int(len(W)/isubsmpl),isubsmpl)).mean(axis=1),
                               int(smoothing/new_dt)).flatten()
 
     return np.arange(len(pLFP))*new_dt, pLFP
     
-def NSI_func(max_low_freqs_power, sliding_mean,
+def NSI_func(low_freqs_envelope, sliding_mean,
              p0=0.,
              alpha=2.):
     """
     p0 should be the 100th percentile of the signal. It can be a sliding mean.
     """
-    X = (p0+alpha*max_low_freqs_power)-sliding_mean # rhythmicity criterion
-    return -2*max_low_freqs_power*heaviside(X)+heaviside(-X)*(sliding_mean-p0)
+    X = (p0+alpha*low_freqs_envelope)-sliding_mean # rhythmicity criterion
+    return -2*low_freqs_envelope*heaviside(X)+heaviside(-X)*(sliding_mean-p0)
 
 
-def Validate_Network_States(data, 
-                            Tstate=200e-3,
-                            Var_criteria=2):
+def compute_sliding_mean(signal, sampling_freq,
+                         T=500e-3):
+    """ just a gaussian smoothing """
+    return gaussian_filter1d(signal, int(T*sampling_freq))
+    
+
+def compute_NSI(signal, sampling_freq,
+                p0=0,
+                low_freqs = np.linspace(2,5,5),
+                T_sliding_mean=500e-3,
+                alpha=2.85):
+    
+    sliding_mean = compute_sliding_mean(signal, sampling_freq, T=T_sliding_mean)
+    
+    low_freqs_envelope = compute_freq_envelope(signal, sampling_freq, low_freqs)
+    
+    return NSI_func(low_freqs_envelope, sliding_mean,
+                    p0=p0,
+                    alpha=alpha)
+    
+    
+def validate_NSI(t_NSI, NSI,
+                 Tstate=200e-3,
+                 var_tolerance_threshold=2):
     
     # validate states:
-    iTstate = int(Tstate/data['new_dt'])
-    # validate the transitions
-    data['NSI_validated'] = np.zeros(len(data['pLFP']), dtype=bool)
-    data['NSI_unvalidated'] = np.zeros(len(data['pLFP']), dtype=bool)
-    for i in np.arange(len(data['pLFP']))[::iTstate][1:-1]:
-        if np.array(np.abs(data['NSI'][i-iTstate:i+iTstate]-data['NSI'][i])<=Var_criteria).all():
-            data['NSI_validated'][i]=True
-        else:
-            data['NSI_unvalidated'][i]=True
+    iTstate = int(Tstate/(t_NSI[1]-t_NSI[0]))
+    NSI_validated = np.zeros(len(NSI), dtype=bool) # false by default
 
-    data['t_validated'] = data['new_t'][data['NSI_validated']]
-    data['i_validated'] = np.arange(len(data['pLFP']))[data['NSI_validated']]
-    
+    # validate the transitions iteratively
+    for i in np.arange(len(NSI))[::iTstate][1:-1]:
+        if np.sum(np.abs(NSI[i-iTstate:i+iTstate]-NSI[i])>var_tolerance_threshold)==0:
+            NSI_validated[i]=True # swith to true
 
-def compute_Network_State_Index(signal,
-                                low_freqs = np.linspace(2,5,5),
-                                Tstate=200e-3,
-                                alpha=2.85,
-                                T_sliding_mean=0.5):
-    
-    # compute sliding mean
-    sliding_mean = gaussian_smoothing(data[key], int(T_sliding_mean/data['new_dt']))
+    return t_NSI[NSI_validated], NSI[NSI_validated]
 
-    # compute low frequency power
-    low_freqs = freqs # storing the used-freq
-    W_low_freqs = my_cwt(data[key].flatten(), freqs, data['new_dt']) # wavelet transform
-    max_low_freqs_power = np.max(np.abs(data['W_low_freqs']), axis=0) # max of freq.
 
-    return NSI_func(max_low_freqs_power, )
-    data['NSI']= Network_State_Index(data,
-                                     p0 = data['p0'],
-                                     alpha=alpha)
-    
-    Validate_Network_States(data,
-                            Tstate=Tstate,
-                            # Var_criteria=Var_criteria,
-                            Var_criteria=data['p0'])
-    
-    
     
 if __name__=='__main__':
 
-    import matplotlib.pylab as plt
-    import NSI
+    import numpy as np
+    import nsi # the NSI module
     
     # -- let's build a fake LFP signal array (having the code features of an awake LFP signal)
-    tstop, dt = 5, 1e-3 # 10s @ 1kHz
+    tstop, dt, sbsmpl_dt = 5, 1e-3, 5e-3 # 10s @ 1kHz
     t = np.arange(int(tstop/dt))*dt
     oscill_part = ((1-np.cos(2*np.pi*3*t))*np.random.randn(len(t))+4*(np.cos(2*np.pi*3*t)-1))*\
         (1-np.sign(t-2))/2.*(2-t)/(tstop-2)
     desynch_part = (1-np.sign(2-t))/2*(t-2)/(tstop-2)*2*np.random.randn(len(t))
-    LFP = (oscill_part+desynch_part) # a ~ 1mV ammplitude signal
+    LFP = (oscill_part+desynch_part)*.1 # a ~ 1mV ammplitude signal
 
     # -- compute the pLFP first
-    t_pLFP, pLFP = NSI.compute_pLFP(1e3*LFP, 1./dt,
+    t_pLFP, pLFP = nsi.compute_pLFP(1e3*LFP, 1./dt,
                                     freqs = np.linspace(50,300,10),
-                                    new_dt=5e-3,
+                                    new_dt=sbsmpl_dt,
                                     smoothing=42e-3)
+    p0 = np.percentile(pLFP, 0./100) # first 100th percentile
 
     # -- then compute the NSI from the pLFP
-    # NSI = NSI.compute_NSI(pLFP,
-    #                       freqs = np.linspace(50, 300, 10))
+    NSI = nsi.compute_NSI(pLFP, 1./sbsmpl_dt,
+                          low_freqs = np.linspace(2, 5, 4),
+                          p0=p0,
+                          alpha=2.85)
 
-    # plot
+    # then validate NSI episodes
+    tvNSI, vNSI = nsi.validate_NSI(t_pLFP, NSI,
+                                   var_tolerance_threshold=20*p0) # here no noise so we increase the thresh
+
+    
+    # let's plot the result
+    import matplotlib.pylab as plt
     fig, ax = plt.subplots(3, 1, figsize=(12,4))
     ax[0].plot(t, LFP, color=plt.cm.tab10(7))
     ax[1].plot(t_pLFP, pLFP, color=plt.cm.tab10(5))
-    # ax[2].plot(t_NSI, NSI)
+    ax[2].plot(t_pLFP, NSI, label='raw')
+    ax[2].plot(tvNSI, vNSI, 'o', label='validated', lw=0)
+    ax[2].legend(frameon=False)
+    
     for x, label in zip(ax, ['LFP (mV)', 'pLFP (uV)', 'NSI (uV)']):
         x.set_ylabel(label)
+        if 'NSI'in label:
+            x.set_xlabel('time (s)')
+        else:
+            x.set_xticklabels([])
+
     plt.show()
     
